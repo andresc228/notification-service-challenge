0bfd773130d42054b7d546be21dd9d570dae19cd
andres <a.camilo0806@gmail.com>
2026-04-30 18:35:40 -0500

    commit class consoleChannel

--- app/model/notification.py
+++ app/model/notification.py
@@ -24,3 +24,17 @@ class NotificationChannel(ABC):
 
     @abstractmethod
     def is_available(self) -> bool: ...
+
+class ConsoleChannel(NotificationChannel):
+
+    def send(self, message: str) -> None:
+        try:
+            print(message)
+        except Exception:
+            raise DeliveryError("Error al imprimir en consola")
+
+    def get_channel_name(self) -> str:
+        return "console"
+
+    def is_available(self) -> bool:
+        return True
